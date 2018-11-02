ef7f3150020f0c38995fd301682c9671accbaa7d
Jet Wu <719191340@qq.com>
2018-11-02 18:19:27 +0800

    -基本配置

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
---
+++ src/pages/layout/layout.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="main_layout">
+		<!-- 左边导航栏 -->
+		<siderbar></siderbar>
+
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return{
+
+			}
+		}
+	}
+</script>
+<style lang="less" scoped>
+	.main_layout{
+
+	}
+</style>
